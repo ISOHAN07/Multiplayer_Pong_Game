da6b9ffbaa6698cf1b3ce50b1ae711ad1ed31df3
Sohan Ghosh <isohan2007@gmail.com>
2025-07-09 16:23:52 +0530

    first commit n socket setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const PORT = 3000;
+server.listen(PORT);
+console.log("listening on port 3000...");
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+});
